a0b45bd4c9ce5a7cd60810c7a0105238f85a88b3
akaruitori <dsabh@ya.ru>
2018-02-09 03:42:30 +0300

    Add russian locale

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module TestGuru
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.time_zone = 'Moscow'
+
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ru
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
