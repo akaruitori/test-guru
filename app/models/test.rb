08169ecebc2cb4f8e0d2e2a63766b75bf8c041f1
akaruitori <dsabh@ya.ru>
2018-02-16 01:26:11 +0300

    Add select_by_category class method to Test model

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,10 @@
 class Test < ApplicationRecord
+  def self.select_by_category(category_title)
+    self.
+      joins('JOIN categories ON tests.category_id = categories.id').
+      where('categories.title = ?', category_title).
+      select('tests.title').
+      order('tests.title DESC').
+      pluck(:title)
+  end
 end
